8fe103ea1dd37b04075545c421daac474c25050f
Ruan Patrick <ruanpatrick.s@hotmail.com>
2023-10-31 20:49:47 -0300

    Begin API

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import express, { Request, Response } from 'express';
-import exampleResponse, { exampleRequest } from './example-to-import';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    console.log("Hello word.");
-    // req.body
-    // req.query
-    // req.params
-    // req.header
-    // req.cookies
-    // req.headers.authorization
-    res.json({
-        message: "Hello world."
-    })
-});
+/**
+ * @Swagger
+ *  "user": 
+ *      id: Integer
+ *      name: String
+ */
+
+interface alsd {
+
+}
 
 app.listen(3000, function () {
     console.log('Ready')
